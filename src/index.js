912094b4c042de11df163aec67d4b33abab5d293
ramazankarisan <rkarisan25@gmail.com>
2022-04-20 01:09:18 +0200

    create and connect redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import userReducer from './redux/userReducer';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+
+
+
+const store = createStore(userReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 
---
+++ src/redux/userReducer.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+
+}
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+
+    default:
+      return state
+  }
+}
+
+export default userReducer
